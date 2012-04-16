655f6d4ede8c89addef4313b99d6b0e5cff51084
Vinod Kurup <vinod@kurup.com>
2012-04-16 16:14:55 -0400

    My final version: 6 seconds
    Stepping appropriately is such a big deal. Without the n-incrementing
    by 2 each time and the next incrementing by 'next' each time, this is
    much much slower.

--- 10.py
+++ 10.py
@@ -3,13 +3,17 @@
 # Calculate the sum of primes below 2,000,000
 import math
 
-def is_prime(n):
-    for i in range(2,int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
+def sum_primes_below(n):
+    prime = [True] * n
+    next = 3
+    sum = 2
+    while next < n:
+        if prime[next]:
+            sum += next
+            # amazing how much difference step makes
+            for i in range(next,n,next):
+                prime[i] = False
+        next += 2
+    return sum
 
-def sum_primes_below_n(n):
-    return sum(filter(is_prime,range(2,n)))
-
-print sum_primes_below_n(2000000)
+print sum_primes_below(2000000)
