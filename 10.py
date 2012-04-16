daf9523603a57dd070158cd23e0bbe204295c400
Vinod Kurup <vinod@kurup.com>
2012-04-15 22:23:39 -0400

    first shot at problem 10

---
+++ 10.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+# Calculate the sum of primes below 2,000,000
+
+def sum_primes_below(n):
+    sum = 0
+    possibles = range(2,n)
+    while len(possibles) > 0:
+        next_prime = possibles[0]
+        sum += next_prime
+        possibles = [i for i in possibles if i%next_prime != 0]
+    return sum
+
+print str(sum_primes_below(10))
+
+
